91b53c63e5ad7afd5a86da6b7b05d85deda1503a
SalimovUz <salimofftolqin7@gmail.com>
2024-02-01 16:38:38 +0500

    Middle

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-const BASE_URL = "http://api.alquran.cloud/v1/";
+// const BASE_URL = "http://api.alquran.cloud/v1/";
 
-const container = document.getElementById("container")
-
-
-const fetchData = async () => {
-  try {
-    const res = await fetch(`${BASE_URL}quran/ar.alafasy`)
-    if(!res.ok) throw new Error("Couldn't refresh data")
-    return res.json()
-  }catch(error) {
-    return error.message
-  }
-}
+// const container = document.getElementById("container")
 
 
-fetchData().then((res)=> {
-  console.log(res);
-  res.data.surahs.map((item, index)=> {
-    
-    // console.log(index.ayahs);
-  })
-})
+// const fetchData = async () => {
+//   try {
+//     const res = await fetch(`${BASE_URL}quran/ar.alafasy`)
+//     if(!res.ok) throw new Error("Couldn't refresh data")
+//     return res.json()
+//   }catch(error) {
+//     return error.message
+//   }
+// }
+
+
+
+// const myUl = document.getElementById("myUl")
+
+
